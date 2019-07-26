e0743a36bcd7deb15a6f6a02a2f1f2da5f116451
Jens W. Klein <jk@kleinundpartner.at>
2019-07-26 14:53:28 +0200

    minor cleanup

--- src/collective/easyformplugin/createdx/actions.py
+++ src/collective/easyformplugin/createdx/actions.py
@@ -1,30 +1,24 @@
 from collective.easyform.actions import Action
 from collective.easyform.actions import ActionFactory
 from collective.easyform.api import get_context
-from collective.easyform.api import get_schema
-from collective.easyform.api import OrderedDict
 from collective.easyformplugin.createdx.interfaces import ICreateDX
 from zope.interface import implementer
-from collective.easyform.api import get_schema
-from collective.easyform.interfaces.savedata import ISaveData
 from plone.supermodel.exportimport import BaseHandler
 from collective.easyformplugin.createdx import _
 
 
 @implementer(ICreateDX)
 class CreateDX(Action):
-    """  """
+    """Create a Dexterity Type"""
 
     def __init__(self, **kw):
         for i, f in ICreateDX.namesAndDescriptions():
             setattr(self, i, kw.pop(i, f.default))
         super(CreateDX, self).__init__(**kw)
 
-
     def createDXItem(self, fields, request, context):
-      """
-      """
-
+        """
+        """
 
     def onSuccess(self, fields, request):
         """
--- src/collective/easyformplugin/createdx/interfaces.py
+++ src/collective/easyformplugin/createdx/interfaces.py
@@ -1,22 +1,6 @@
 # -*- coding: utf-8 -*-
-import zope.schema.interfaces
-import zope.interface
-import zope.i18nmessageid
-from z3c.form.browser.textarea import TextAreaWidget
-from z3c.form.browser.checkbox import CheckBoxFieldWidget
-from plone.supermodel.model import fieldset
-from plone.schema import Email
-from plone.autoform import directives
-from plone.app.textfield import RichText
 from collective.easyform.interfaces import IAction
-from collective.easyform import config
-from collective.easyform.actions import IAction
-from collective.easyform import vocabularies
-from collective.easyformplugin.createdx import _
-from plone import schema
 
 
 class ICreateDX(IAction):
-
-    """
-    """
+    """Create a Dexterity Type"""
