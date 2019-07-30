6d02ba30be1935b06c2a80d480a629d9c61e7614
Peter Holzer <peter.holzer@agitator.com>
2019-07-30 18:04:22 +0200

    remove test support for < 5.1

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ setup(
         "Environment :: Web Environment",
         "Framework :: Plone",
         "Framework :: Plone :: Addon",
+        "Framework :: Plone :: 5.1",
         "Framework :: Plone :: 5.2",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
