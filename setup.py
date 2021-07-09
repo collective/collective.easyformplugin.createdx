e33bed4edaf6acfd65adf1ba98b0c4815c3f36d7
Peter Holzer <peter.holzer@agitator.com>
2021-07-09 13:16:51 +0200

    Back to development: 1.0a4

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='collective.easyformplugin.createdx',
-    version='1.0a3',
+    version='1.0a4.dev0',
     description="Create dexterity objects from easyform submissions",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
